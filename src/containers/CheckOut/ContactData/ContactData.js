746fceab25aad5b8fbd88d3fa51af49f95e2262b
Colin Hale <Halecolin1@gmail.com>
2019-05-26 11:21:12 -0600

    updated App

--- src/containers/CheckOut/ContactData/ContactData.js
+++ src/containers/CheckOut/ContactData/ContactData.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+
 import Button from '../../../components/UI/Button/Button';
-import classes from './ContactData.css'
-import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -10,60 +11,57 @@ class ContactData extends Component {
         email: '',
         address: {
             street: '',
-            postalCode: '',
-        }, 
-        loading:false
+            postalCode: ''
+        },
+        loading: false
     }
-    orderHandler = (event) => {
+
+    orderHandler = ( event ) => {
         event.preventDefault();
-        this.setState({loading: true})
+        this.setState( { loading: true } );
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
             customer: {
-                name: 'Colin Hale',
+                name: 'Max Schwarzmüller',
                 address: {
-                    street: 'TestStreet 1',
-                    zipCode: '84601',
-                    country: 'USA'
-
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Germany'
                 },
-                email: 'TestEmail@gmail.com',
-                deliveryMethod: 'fastest'
-
-            }
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
         }
-
-        axios.post('/orders.json', order)
-            .then(response => {
-                console.log(response);
-                this.setState({loading: false})
-                this.props.history.push('/')
-            })
-            .catch(error => {
-                console.log(error)
-                this.setState({loading: false})
-            });
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+                this.props.history.push('/');
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
     }
-    render(){
+
+    render () {
         let form = (
             <form>
-                <input type="text" name="name" placeholder="Your Name" />
-                <input type="text" name="email" placeholder="Your Email" />
-                <input type="text" name= "street" placeholder="Street" />
-                <input type="text" name= "postal" placeholder="Post Code" />
-                <Button btnType = "Success" clicked = {this.orderHandler}>ORDER</Button>
-            </form>);
-        if(this.state.loading){
-            form = <Spinner />
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
         }
         return (
             <div className={classes.ContactData}>
-                <h4>Enter your contact data.</h4>
+                <h4>Enter your Contact Data</h4>
                 {form}
-
             </div>
-        )
+        );
     }
 }
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,34 +1,38 @@
-// eslint-disable-next-line react/no-typos
-import React , { Component }from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../Aux/Aux';
-import classes from './Layout.css'
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
         showSideDrawer: false
     }
-    sideDrawerCloseHandler = () => {
-        this.setState({showSideDrawer: false});
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
     }
-    sideDrawerOpenHandler = () => {
-        this.setState({showSideDrawer: true});
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
     }
-    render() {
-        return (  
+
+    render () {
+        return (
             <Aux>
-                <Toolbar open = {this.sideDrawerOpenHandler} />
-                <SideDrawer 
-                    closed = {this.sideDrawerCloseHandler}
-                    open = {this.state.showSideDrawer} />
-                <div>Toolbar Sidebar, Backdrop</div>
-                <main className = {classes.Content}>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
                     {this.props.children}
                 </main>
             </Aux>
         )
     }
-} ;
+}
 
 export default Layout;
